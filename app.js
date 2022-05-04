0c1a904deaaa1138b1e4fc183c30121aed1f4e40
takayama <takayama@foxmail.com>
2022-05-04 18:26:35 +0900

    -

---
+++ app.js
@@ -0,0 +1,88 @@
+const $ = document.querySelector.bind(document)
+const Buffer = buffer.Buffer
+
+$("#new-tab").href = location.href
+
+function contentChangeListener () {
+	this.value = prettifyHex(this.value)
+	const cnt = String(this.value).replace(/\s/g, "").length / 2
+	$("#bytes-cnt").innerHTML = String(Math.floor(cnt))
+}
+$("#content").onkeyup = contentChangeListener
+$("#content").onmouseup = contentChangeListener
+$("#content").onchange = contentChangeListener
+
+function keyChangeListener () {
+	this.value = prettifyHex(this.value)
+}
+$("#key").onkeyup = keyChangeListener
+$("#key").onmouseup = keyChangeListener
+$("#key").onchange = keyChangeListener
+
+function pbReplacer(k, v) {
+	if (k !== "" && v instanceof pb.Proto) {
+		v.__proto__.encoded = v.encoded
+		return v.__proto__
+	}
+	if (k === "encoded")
+		return prettifyHex(Buffer.from(v).toString("hex")) + ("(" + Buffer.from(v).toString() + ")")
+	return v
+}
+
+$("#do").onclick = function () {
+	const type = $("#type").value
+	const content = $("#content").value.replace(/\s/g, "")
+	const key = $("#key").value.replace(/\s/g, "")
+	const contentBuffer = Buffer.from(content, "hex")
+	const keyBuffer = Buffer.from(key, "hex")
+	let resultBuffer, result
+	try {
+		switch (type) {
+		case "UnProtobuf":
+			resultBuffer = pb.decode(contentBuffer)
+			delete resultBuffer.encoded
+			result = JSON.stringify(resultBuffer, pbReplacer, 2).replace(/\n/g, "<br>")
+			break
+		case "UnQQTea":
+			resultBuffer = tea.decrypt(contentBuffer, keyBuffer)
+			result = prettifyHex(resultBuffer.toString("hex"))
+			break
+		case "EnQQTea":
+			resultBuffer = tea.encrypt(contentBuffer, keyBuffer)
+			result = prettifyHex(resultBuffer.toString("hex"))
+			break
+		default:
+			result = `<span style="color:red">Error: unimplemented</span>`
+			break
+		}
+	} catch (e) {
+		result = `<span style="color:red">Error: ${e.message}</span>`
+	}
+	$("#result").innerHTML = String(result)
+}
+
+function prettifyHex(hex) {
+	return hex.replace(/\s/g, "").replace(/(.)(.)/g, '$1$2 ')
+}
+
+function hex2bytes(hex = "") {
+	const bytes = []
+	for (let i = 0; i < hex.length; i += 2)
+		bytes.push(parseInt(hex.substr(i, 2), 16))
+	return bytes
+}
+
+function hex2str(hex) {
+	let str = ""
+	for (let i = 0; i < hex.length; i += 2)
+		str += "%" + hex.substr(i, 2)
+	return decodeURIComponent(str);
+}
+
+// {
+// 	const bytes = hex2bytes(content)
+// 	while (bytes.length < 4)
+// 		bytes.unshift(0)
+// 	const dv = new DataView(new Uint8Array(bytes).buffer)
+// 	result = dv.getUint32()
+// }
